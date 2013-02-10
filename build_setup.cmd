e431f6c63ee2e331244015992caff97a258d66e7
utapyngo <utapyngo@gmail.com>
2013-02-10 20:49:27 +0700

    Renamed installer to setup_ppt_rasterize.bat

--- build_setup.cmd
+++ build_setup.cmd
@@ -1,5 +1,5 @@
 @echo off
-set "outfile=setup_ppt_rasterize.cmd"
+set "outfile=setup_ppt_rasterize.bat"
 set "files=register.cmd ppt_rasterize.ps1 ppt_rasterize.cmd uninstall.cmd unreg.ps1"
 
 echo @echo off>%outfile%
@@ -8,15 +8,10 @@ echo set "files=%files%">>%outfile%
 echo.>>%outfile%
 type install.cmd>>%outfile%
 
-echo ::begin filelist>>%outfile%
-for %%f in (%files%) do (
-    echo %%f>>%outfile%
-)
-echo ::end filelist>>%outfile%
-
 for %%f in (%files%) do (
     echo.>>%outfile%
     echo ::begin %%f>>%outfile%
     type %%f>>%outfile%
+    echo.>>%outfile%
     echo ::end %%f>>%outfile%
 )
