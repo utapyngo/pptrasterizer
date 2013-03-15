e21dce94a011cd3de0e7edeaa683d1479fa49466
utapyngo <utapyngo@gmail.com>
2013-03-15 10:55:20 +0700

    Fixed "The system cannot find the file specified"
    It was the first line when running the installer.

--- vars.cmd
+++ vars.cmd
@@ -2,4 +2,6 @@ set "product_name=PowerPoint Presentation Rasterizer"
 set "short_name=pptrasterizer"
 set "installer_name=setup_%short_name%.bat"
 set "install_dir=%~dp0"
-set /p version=<"%install_dir%version.txt"
+if exist "%install_dir%version.txt" (
+    set /p version=<"%install_dir%version.txt"
+)
